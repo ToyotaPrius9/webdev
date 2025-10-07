27a0c3a92038a14bcb80b20ad39733a7d22d44b2
ToyotaPrius9 <harman.singh@binus.ac.id>
2025-10-07 23:03:09 +0700

    Added tutorial video in About page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,7 +7,7 @@ export default function AboutPage() {
       <p>Name: {studentInfo.name}</p>
       <p>Student Number: {studentInfo.number}</p>
       <video controls width="400" className="mt-4 rounded border">
-        <source src="/demo.mp4" type="video/mp4" />
+        <source src="/TutorialAssignment1.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     </section>
